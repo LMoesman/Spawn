aaf5715bf858cd8865a1de37ea012320827ffec8
Jake Heiser <jakeheiser1@gmail.com>
2016-10-27 22:26:42 -0700

    Removing demo from package

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "Spawn",
-    targets: [Target(name: "SpawnDemo", dependencies: ["Spawn"])]
+    name: "Spawn"
 )
--- Sources/SpawnDemo/main.swift
+++
@@ -1,9 +0,0 @@
-import Spawn
-
-do {
-    let spawn = try Spawn(args: ["/bin/sh", "-c", "ls", "."]) { str in
-        print(str)
-    }
-} catch {
-    print("error: \(error)")
-}

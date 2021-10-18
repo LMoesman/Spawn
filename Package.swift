99311630a9ca31583501b5bee3c41f17b3835c72
Lars Moesman <>
2021-10-18 20:36:00 +0200

    Fix error: package 'spawn' contains no products

--- Package.swift
+++ Package.swift
@@ -4,4 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "Spawn"
+    products: [
+        .library(name: "Spawn", targets: ["Spawn"])
+    ],
+    targets: [
+        .targets(name: "Spawn", dependencies: [])
+    ]
 )
